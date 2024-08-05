7b6626c9163142d4a6b36cfb691d79678c00e573
ChristKomika <christkomika7@gmail.com>
2024-08-05 11:56:22 +0100

    Premier commit de mon tutoreiel nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import "@/app/ui/global.css";
+
+import { inter } from "./ui/font";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="fr">
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
